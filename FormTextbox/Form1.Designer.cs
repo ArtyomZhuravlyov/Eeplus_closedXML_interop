298ca9571abfb4f4b3e1d5b6088b1d3f9d494406
Артём Журавлёв <artembestnet@mail.ru>
2019-04-30 12:22:39 +0300

    start

---
+++ FormTextbox/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FormTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(69, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(287, 97);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Загрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(467, 94);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(90, 252);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker2_KeyDown_1);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(332, 287);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker2.TabIndex = 5;
+            this.dateTimePicker2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker2_KeyDown_1);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker3.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.dateTimePicker3.Location = new System.Drawing.Point(440, 361);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker3.TabIndex = 6;
+            this.dateTimePicker3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker2_KeyDown_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.numericUpDown1.Location = new System.Drawing.Point(114, 384);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ThousandsSeparator = true;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(90, 324);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker4.TabIndex = 8;
+            this.dateTimePicker4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker2_KeyDown_1);
+            // 
+            // dateTimePicker5
+            // 
+            this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker5.Location = new System.Drawing.Point(573, 311);
+            this.dateTimePicker5.Name = "dateTimePicker5";
+            this.dateTimePicker5.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker5.TabIndex = 9;
+            this.dateTimePicker5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker2_KeyDown_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker5);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker5;
+    }
+}
+
---
+++ FormTextbox/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTextbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Text = "https://docs.microsoft.com/ru-ru/dotnet/api/system.windows.forms.richtextbox.linkclicked?view=netframework-4.8";
+            LinkLabel link1 = new LinkLabel();
+           // richTextBox1.DetectUrls = false;
+            link1.Text = "наука";
+            // link1.LinkClicked += new LinkLabelLinkClickedEventHandler(RichTextBox1_LinkClicked);
+            richTextBox1.LinkClicked += Link_Clicked;
+            // dateTimePicker1
+            dateTimePicker4.KeyDown += dateTimePicker2_KeyDown_1;
+
+            Form form = new Form();
+            form.IsMdiContainer = true;
+            MenuStrip menuStrip1 = new MenuStrip();
+            menuStrip1.Items.Add("sdf");
+            this.IsMdiContainer = true;
+
+            // form.MainMenuStrip.Items.Add("MainMenuStrip");
+            this.Controls.Add(menuStrip1);
+               form.Show();
+
+            Form form2 = new Form();
+            form2.MdiParent = form;
+
+            var pp = form2.WindowState;
+            form2.Icon = Properties.Resources.Tuziibanez_Profesional_Red_Network_drive_connected;
+            form2.WindowState = FormWindowState.Maximized;
+            form2.Show();
+
+            Form form3 = new Form();
+            form3.MdiParent = form;
+
+            var pp3 = form3.WindowState;
+            form3.Icon = Properties.Resources.Tuziibanez_Profesional_Red_Network_drive_connected;
+            form3.WindowState = FormWindowState.Maximized;
+            //  form3.ShowIcon = false;
+            // form3.Icon = SystemIcons.Application;
+            //  form3.ControlBox = false;
+            //  form3.ShowInTaskbar = false;
+            form3.Show();
+
+            //Form form4 = new Form();
+            //form4.Icon = Properties.Resources.Tuziibanez_Profesional_Red_Network_drive_connected;
+            //form4.WindowState = FormWindowState.Maximized;
+            ////  form3.ShowIcon = false;
+            //// form3.Icon = SystemIcons.Application;
+            ////  form3.ControlBox = false;
+            //form4.MdiParent = this;
+            //form4.Show();
+            ////    form.MainMenuStrip.Items.RemoveAt(0);
+
+
+            // form2.StartPosition = FormStartPosition.CenterParent;
+        }
+
+        private void Link_Clicked(object sender, System.Windows.Forms.LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+        }
+
+        private void dateTimePicker1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.Control &&  e.KeyCode == Keys.C)
+            {
+                // Clipboard.SetText(((DateTimePicker)sender).Value.ToString("d"));
+                Clipboard.SetText(((DateTimePicker)sender).Value.ToString());
+                e.SuppressKeyPress = true; //избавляет от звука винды при некорректном действии
+            }
+
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                try
+                {
+                   // Clipboard.SetText(((DateTimePicker)sender).Value.ToString("d"));
+                     //    ((DateTimePicker)sender).Value = (DateTime)Clipboard.GetData("d");
+                    ((DateTimePicker)sender).Value = Convert.ToDateTime(Clipboard.GetText());
+                    e.SuppressKeyPress = true; //избавляет от звука винды при некорректном действии
+                }
+                catch
+                {
+                    
+                }
+
+                
+            }
+        }
+
+
+        private void dateTimePicker2_KeyDown_1(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                switch (((DateTimePicker)sender).Format)
+                {
+                    case DateTimePickerFormat.Time:
+                        Clipboard.SetText(((DateTimePicker)sender).Value.ToString("T")); break;
+                    case DateTimePickerFormat.Long:
+                        Clipboard.SetText(((DateTimePicker)sender).Value.ToString("D"));  break;
+                    case DateTimePickerFormat.Short:
+                        Clipboard.SetText(((DateTimePicker)sender).Value.ToString("d")); break;
+                    default:
+                        Clipboard.SetText(((DateTimePicker)sender).Value.ToString()); break;
+                }
+                //   Clipboard.SetText(((DateTimePicker)sender).Value.ToString("d"));
+              //  Clipboard.SetText(((DateTimePicker)sender).Value.ToString());
+            e.SuppressKeyPress = true; //избавляет от звука винды при некорректном действии
+            }
+
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+            //  Clipboard.SetText(((DateTimePicker)sender).Value.ToString("d"));
+            // ((DateTimePicker)sender).Value = (DateTime)Clipboard.GetData("d");
+                try
+                {
+                    ((DateTimePicker)sender).Value = Convert.ToDateTime(Clipboard.GetText());
+                    e.SuppressKeyPress = true; //избавляет от звука винды при некорректном действии
+                }
+                catch
+                {
+
+                }
+            
+            }
+        }
+        
+    }
+}
